f7ed77a85f80395310d5c447002443060f76d52f
CrypticSwarm <crypticswarm+git@gmail.com>
2012-11-07 23:58:48 -0600

    slightly better testing

--- test.js
+++ test.js
@@ -1,14 +1,32 @@
+var test = require('tap').test
 var MemoryTree = require('./memory')
 
-var a = { a: {}, b: {} }
-a = new MemoryTree(a)
+test('match vars', function (t) {
+  var init = { a: {}, b: {} }
+  a = new MemoryTree(init)
+  t.same(a.value, { a: {}, b: {} })
 
-a.set('a.b', 7)
-.set('b.b', 9)
-.set('a.c', 43)
-.set('a.d', 97)
-.set('b.z', 9)
-.set('c.z', 84).previous.previous.set('j.z', 92)
+  a = a.set('a.b', 7)
+  t.same(a.value, { a: { b: 7 } })
+
+  a = a.set('b.b', 9)
+  t.same(a.value.a, { b: 7 })
+  t.same(a.value, { b: { b: 9 } })
+
+  a = a.set('a.c', 43)
+  t.same(a.value, { a: { c: 43 } })
+
+  a = a.set('a.d', 97)
+  t.same(a.value, { a: { d: 97 } })
+
+  a = a.set('c.z', 84).previous.previous
+  t.same(a.value, { a: { c: 43 } })
+  t.same(a.next.value, { a: { d: 97 } })
+
+  t.end()
+})
+
+/*
 print(a.previous.value)
 console.log(a.value)
 print(a.tnext.value)
@@ -19,7 +37,7 @@ function print(obj, sp) {
   if ( typeof obj == 'object') {
     process.stdout.write(sp + '{ ')
     for(var i in obj) {
-      process.stdout.write(sp + delim + i + ':') 
+      process.stdout.write(sp + delim + i + ':')
       if (typeof obj[i] == 'object') process.stdout.write('\n')
       print(obj[i], sp + '  ')
       delim = ', '
@@ -28,3 +46,4 @@ function print(obj, sp) {
   }
   else console.log(sp + obj)
 }
+*/
